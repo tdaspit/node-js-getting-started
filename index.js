a0f867e669c49382ee6a7a32e14252cf3e0d3f55
Tyler Daspit <54476633+tdaspit@users.noreply.github.com>
2022-03-18 11:02:29 -0500

    changed index.js

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+/*
 const cool = require('cool-ascii-faces');
+const times = require(showTimes())
+*/
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const user = {
+    name: 'Jack',
+    isMarried: false,
+    age: 25,
+}
+ 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/', (req, res) => res.send(user))
+/*
+.get('/cool', (req, res) => res.send(cool()))
+.get('/times', (req, res) => res.send(showTimes()))
+*/
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+/*
+showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
+*/
